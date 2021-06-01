d6feb5d27fb29ee1c8473511d1f4649023721bd5
= <jose.souza@zup.com.br>
2021-06-01 15:57:04 -0300

    11.autenticacao_autorizacao_oauth

--- src/main/java/br/com/zupacademy/transacoes/transacao/TransacaoController.java
+++ src/main/java/br/com/zupacademy/transacoes/transacao/TransacaoController.java
@@ -1,12 +1,5 @@
 package br.com.zupacademy.transacoes.transacao;
 
-import br.com.zupacademy.transacoes.TokenService;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.OidcKeycloakAccount;
-import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,12 +7,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotBlank;
 
 @RestController
@@ -27,14 +20,14 @@ import javax.validation.constraints.NotBlank;
 public class TransacaoController {
 
     @Autowired TransacaoRepository repository;
-    @Autowired TokenService tokenService;
 
     @GetMapping("/cartao/{id}")
     public ResponseEntity<?> listarUltimasCompras(
+            @AuthenticationPrincipal Jwt jwt,
             @NotBlank @PathVariable("id") String id,
             @PageableDefault(size = 10, direction = Sort.Direction.DESC, sort = "efetivadaEm") Pageable pageable) {
 
-        String email = tokenService.getEmail();
+        String email = jwt.getClaimAsString("email");
 
         Page<Transacao> transacoes = repository.findByCartaoIdAndCartaoEmail(id, email, pageable);
 
